bddbe16919e1d397e6569ba2968159db8b7a6ec1
ScoobyLuffyDoo <ScoobyLuffyDoo@gmail.com>
2022-05-02 14:43:01 +0200

    Added Secerets

---
+++ secrets.py
@@ -0,0 +1,10 @@
+secrets = {
+    'client_secret': "K-GSp84z9s009oeJ7SKCuGX4ykIUfA",
+    'name': "MemeReporter",
+    'redirect_uri': 'http://127.0.0.1',
+    'username': 'BobThePythonBuilder ',
+    'password':'',
+    'client_id':'VnnuNqwDnbosnV9S-fsB6w',
+    'user_agent':"MemeReporter by /u/BobThePythonBuilder"
+  
+}
