7f16eead319d4ebd6f40b8758d24d8a8c80a00a0
ScoobyLuffyDoo <ScoobyLuffyDoo@gmail.com>
2022-05-02 14:41:50 +0200

    Reddit Function

--- index.py
+++ index.py
@@ -23,16 +23,31 @@ reddit = praw.Reddit(
     password=secrets['password']
 )
 
-# subreddit = r.subreddit("meme")
-count = 0
-limmit_amount =30
-for submission in reddit.subreddit("meme").top(limit=limmit_amount):
-    title = submission.title
-    url = submission.url
-    if url.endswith("jpg") or url.endswith("jpeg") or url.endswith("png") or url.endswith("gif"):
-        print(url)
-        filetype = fileExtention(str(url))
-        urllib.request.urlretrieve(url, f"./memes/image{count}.{filetype}")
-        count += 1
-        if count == limmit_amount:
-           break
+def topMemeDownloader(active =None):
+    count = 0
+    limmit_amount =30
+    if active == True:
+        for memes in reddit.subreddit("meme").top(limit=limmit_amount):
+            # title = submission.title
+            url = memes.url
+            if url.endswith("jpg") or url.endswith("jpeg") or url.endswith("png") or url.endswith("gif"):
+                print(url)
+                filetype = fileExtention(str(url))
+                urllib.request.urlretrieve(url, f"./memes/image{count}.{filetype}")
+                count += 1
+                if count == limmit_amount:
+                    break
+
+
+def learnPythonTopics(active = None):
+    limmit_amount = 20
+    if active == True:
+        for count, topics in enumerate(reddit.subreddit("Python").hot(limit=limmit_amount)):
+            title = topics.title
+            url = topics.url
+            print(count,title,url)
+
+
+if __name__ == "__main__":
+    topMemeDownloader(False)
+    learnPythonTopics(True)
